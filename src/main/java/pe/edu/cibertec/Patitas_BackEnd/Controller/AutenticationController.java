1b1f26d17e8301a5aae5ffdb5af0e822f89632fd
Jheicer <rodriguezolivaresjheicer@gmail.com>
2024-09-24 22:12:03 -0500

    Consumo api buenas practicas

--- src/main/java/pe/edu/cibertec/Patitas_BackEnd/Controller/AutenticationController.java
+++ src/main/java/pe/edu/cibertec/Patitas_BackEnd/Controller/AutenticationController.java
@@ -11,6 +11,8 @@ import pe.edu.cibertec.Patitas_BackEnd.service.AuthenticationService;
 import pe.edu.cibertec.Patitas_BackEnd.service.implementation.AuthenticationServiceImpl;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/autentication")
@@ -22,15 +24,16 @@ public class AutenticationController {
     @PostMapping("/inicio")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
         try {
+            Thread.sleep(Duration.ofSeconds(45));
             String[] datosUsuario = authenticationService.validarUsuario(loginRequestDTO);
+            System.out.println(Arrays.toString(datosUsuario));
             if (datosUsuario == null) {
                 return new LoginResponseDTO("01", "Error: Usuario no encontrado", "", "");
             }
 
-            // Asegúrate de usar los índices correctos
             return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1]); // nombre y correo
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             return new LoginResponseDTO("99", "Error: Ocurrió un problema", "", "");
         }
     }
